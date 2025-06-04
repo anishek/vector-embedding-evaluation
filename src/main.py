e3fa238ef18815fab47cb9309480124748daf649
Anishek Agarwal <anishek@gmail.com>
2025-06-04 19:58:42 +0530

    no gpu compatibility on my machine so model running on cpu , did prarallelization with processPoolExecutor but the run breaks in between on my cpu with no errors have to debug next if possible, the time taken is still too long so may be not worth doing even embedding unless u have access to powerful infra

--- src/main.py
+++ src/main.py
@@ -7,27 +7,47 @@ from sentence_transformers import SentenceTransformer
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from file_management import FileReader
 from db_manager import PostgresManager
+import asyncio
+from concurrent.futures import ProcessPoolExecutor
 
 
 # based on https://huggingface.co/intfloat/multilingual-e5-large-instruct
 # from leaderboard at https://huggingface.co/spaces/mteb/leaderboard
-def e5_large_embedding(input_data, read_from_location):
+async def e5_large_embedding(input_data, read_from_location):
     model = SentenceTransformer("intfloat/multilingual-e5-large-instruct")
     # to get the max token length that the model can process used
     print("Max:", model.max_seq_length)  # Output: 512
     embeddings_to_add = []
-    for text in recursive_chunking(input_data, model.max_seq_length):
-        embedding = model.encode(text)
-        embeddings_to_add.append(
-            {
-                "location": read_from_location,
-                "embedding": embedding,
-                "text_content": text,
-            }
-        )
+    chunked_texts = recursive_chunking(input_data, model.max_seq_length)
+    print("chunked_texts ", len(chunked_texts))
+    with ProcessPoolExecutor() as executor:
+        tasks = [
+            get_embeddings_async(chunked_text, executor)
+            for chunked_text in chunked_texts
+        ]
+        results = await asyncio.gather(*tasks)
+        for result in results:
+            text, embedding = result
+            embeddings_to_add.append(
+                {
+                    "location": read_from_location,
+                    "embedding": embedding,
+                    "text_content": text,
+                }
+            )
     pg_manager = PostgresManager()
     print("embeddings found : ", len(embeddings_to_add))
     pg_manager.insert_embeddings_batch(embeddings_to_add)
+
+
+async def get_embeddings_async(text, executor):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(executor, get_embeddings_sync, text)
+
+
+def get_embeddings_sync(text):
+    model = SentenceTransformer("intfloat/multilingual-e5-large-instruct")
+    return text, model.encode(text)
 
 
 # chunk_size reflects the number of characters that a chunk has, tokens is what the llm model is limited on hence
@@ -46,7 +66,7 @@ def recursive_chunking(text, chunk_size=512):
 def main():
     reader = FileReader()
     read_from_location, content = reader.read_next()
-    e5_large_embedding(content, read_from_location)
+    asyncio.run(e5_large_embedding(content, read_from_location))
 
 
 if __name__ == "__main__":
